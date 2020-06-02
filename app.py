ee30d1f0d28cfe56c1464fb1c087cbe456fff2ba
BrokenShell <webmaster@sharpdesigndigital.com>
2020-06-01 22:09:19 -0700

    Initial commit

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, request
+from pandas import DataFrame
+
+
+app = Flask(__name__)
+
+# placeholder data
+DATA = DataFrame({
+    'Name': ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'],
+    'Rating': [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+})
+
+
+def custom_filter(slider_value: str) -> DataFrame:
+    rate = int(slider_value)
+    return DATA[DATA['Rating'] >= rate]
+
+
+@app.route("/")
+@app.route("/index")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/data-slider")
+@app.route("/data-slider", methods=['GET'])
+def slider():
+    val = request.args.get('val')
+    if val is not None:
+        return render_template(
+            "data-slider.html", val=val, find=custom_filter)
+    else:
+        return render_template("data-slider.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
